37e85f134e2cfff32f768867e06e233643656a36
fcoverdugoa <fcoverdugoa@underpost.net>
2021-04-21 02:37:40 -0400

    update colorize scheme

--- src/scheme/lib/colorize.scm
+++ src/scheme/lib/colorize.scm
@@ -9,3 +9,9 @@
 
 (define py_n (lambda (num) (display (colorize (string-append (number->string num) "\n") 'yellow))))
 (define py_s (lambda (str) (display (colorize (string-append str "\n") 'yellow))))
+
+(define pr_n (lambda (num) (display (colorize (string-append (number->string num) "\n") 'red))))
+(define pr_s (lambda (str) (display (colorize (string-append str "\n") 'red))))
+
+(define pm_n (lambda (num) (display (colorize (string-append (number->string num) "\n") 'magenta))))
+(define pm_s (lambda (str) (display (colorize (string-append str "\n") 'magenta))))
