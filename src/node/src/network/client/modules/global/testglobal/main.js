0d79f994bd7f0a3a6e392b898fa1cb0c35bd54aa
fcoverdugoa <fcoverdugoa@underpost.net>
2021-10-24 18:20:27 -0300

    update

--- src/node/src/network/client/modules/global/testglobal/main.js
+++ src/node/src/network/client/modules/global/testglobal/main.js
@@ -1,4 +1,16 @@
 const testglobal = {
-  loader: async ()=>{ append('body', ' content global <br>'); },
+  loader: async ()=>{
+
+    append('body', ' content global <br>');
+
+    s('body').onkeydown = () => {
+    		switch (window.event.keyCode) {
+    			case 13:
+    					notifi.display('pink', 'test', 1000, 'failed');
+    				break;
+    			default:
+    		}
+    };
+  },
   render: async ()=>{}
 };
