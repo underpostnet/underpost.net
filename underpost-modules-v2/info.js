3b31d1f2e96ce4c8b98f500eefe706b72bf71598
fcoverdugo <fcoverdugoa@underpost.net>
2022-02-16 17:52:11 -0300

    update

---
+++ underpost-modules-v2/info.js
@@ -0,0 +1,50 @@
+import colors from 'colors/safe.js';
+import util from './util.js';
+
+
+const info = {
+  reqData: (req, path, logGroups) => {
+      const reqInfo = {
+        ip: req.connection.remoteAddress || req.headers['x-forwarded-for'],
+        date: new Date().toISOString(),
+        host: req.headers.host,
+        lang: req.acceptsLanguages().join('|'),
+        browser: req.useragent.browser,
+        version: req.useragent.version,
+        os: req.useragent.os,
+        platform: req.useragent.platform,
+        geoIp: util.jsonSave(req.useragent.geoIp)
+      };
+      if(logGroups===true){
+        console.log('');
+        console.log(colors.bgBrightYellow(colors.black(util.tu(' path info '))));
+        console.table(path);
+        console.log(colors.bgBrightYellow(colors.black(util.tu(' req info '))));
+        console.table(reqInfo);
+        console.log(colors.bgBrightYellow(colors.black(util.tu(' source info '))));
+        console.table(req.useragent.source);
+        console.log(colors.bgBrightYellow(colors.black(util.tu(' resume '))));
+      }
+      return {
+        ...path,
+        ...reqInfo,
+        ...{source: req.useragent.source}
+      }
+  },
+  req: (req, path, paths, logGroups) => {
+    const dataPath = paths.filter(x=>x.path==path.uri)[0];
+    const display_ = info.reqData(req, path, logGroups);
+    const source_ = display_.source;
+    delete display_.source;
+    console.log(
+      ' \n > '
+     + colors.bgYellow(colors.black(' '
+     + util.tu(dataPath.methods)
+     + ' ') )+ colors.green(' .'+path.uri)
+     + '\n  '+colors.yellow(display_.date));
+    console.table({ ...dataPath, ...display_ });
+    console.log(' source: '+colors.green(source_));
+  }
+};
+
+export default info;
--- underpost.js
+++ underpost.js
@@ -4,5 +4,6 @@ import util from './underpost-modules-v2/util.js';
 import navi from './underpost-modules-v2/navi.js';
 import rest from './underpost-modules-v2/rest.js';
 import files from './underpost-modules-v2/files.js';
+import info from './underpost-modules-v2/info.js';
 
-export { util, navi, rest, files };
+export { util, navi, rest, files, info };
