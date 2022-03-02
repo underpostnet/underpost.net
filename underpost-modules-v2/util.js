a7860352d79355eb12314a5d03b6116fad8bf115
fcoverdugo <fcoverdugoa@underpost.net>
2022-03-02 12:12:05 -0300

    update

--- underpost-modules-v2/util.js
+++ underpost-modules-v2/util.js
@@ -38,7 +38,8 @@ const util = {
           result += characters.charAt(Math.floor(Math.random() * charactersLength));
        }
        return result;
-    }
+  },
+  isoDateRegex: () => new RegExp('^[0-9]{4}-((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01])|(0[469]|11)-(0[1-9]|[12][0-9]|30)|(02)-(0[1-9]|[12][0-9]))T(0[0-9]|1[0-9]|2[0-3]):(0[0-9]|[1-5][0-9]):(0[0-9]|[1-5][0-9])\.[0-9]{3}Z$')
 };
 
 
