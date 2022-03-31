a6470b098a3111b87f213eeeb4435ea6387f6350
fcoverdugo <fcoverdugoa@underpost.net>
2022-03-31 11:07:01 -0300

    update

--- underpost-modules-v2/util.js
+++ underpost-modules-v2/util.js
@@ -40,6 +40,7 @@ const util = {
        return result;
   },
   reduce: render_ => render_.replace(/\n|\t/g, ''),
+  clearEmpty: render_ => render_.replace(/\s\s/g, ''),
   isoDateRegex: () => new RegExp('^[0-9]{4}-((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01])|(0[469]|11)-(0[1-9]|[12][0-9]|30)|(02)-(0[1-9]|[12][0-9]))T(0[0-9]|1[0-9]|2[0-3]):(0[0-9]|[1-5][0-9]):(0[0-9]|[1-5][0-9])\.[0-9]{3}Z$')
   // getTimeIso: (isso, offTimeFactor) => new Date().getTime() + offTime(),
   // offTime:
