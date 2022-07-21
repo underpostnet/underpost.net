8a723d14ff3349ce41eb47d5e5fde9dc8a1abcc8
fcoverdugo <fcoverdugoa@underpost.net>
2022-07-21 15:25:13 -0400

    update

--- underpost-modules-v1/koyn/class/blockChain.js
+++ underpost-modules-v1/koyn/class/blockChain.js
@@ -996,7 +996,7 @@ export class BlockChain {
 	async currentAmountCalculator(base64PublicKey, log, dataTransaction){
 
 		base64PublicKey = 
-		Buffer.from(base64PublicKey, 'base64')
+		Buffer.from(base64PublicKey, 'base64').toString()
 		.replace(/\r/g, '').toString('base64');
 
 		if(dataTransaction!=undefined){
