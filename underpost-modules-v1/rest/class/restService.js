2700cb77a90b7e4c11f5d146122cf156bb130540
fcoverdugo <fcoverdugoa@underpost.net>
2022-10-11 20:23:43 -0300

    update

--- underpost-modules-v1/rest/class/restService.js
+++ underpost-modules-v1/rest/class/restService.js
@@ -7,92 +7,92 @@ import { Util } from "../../util/class/Util.js";
 
 export class RestService {
 
-  constructor(){}
+  constructor() { }
 
-  async getJSON(url){
-    return await new Promise((resolve)=>{
+  async getJSON(url) {
+    return await new Promise((resolve) => {
       fileGetContents(url).then(json => {
-          // console.log(colors.blue("getJSON( "+url+" ) success ->"));
-          resolve(JSON.parse(json));
+        // console.log(colors.blue("getJSON( "+url+" ) success ->"));
+        resolve(JSON.parse(json));
       }).catch(error => {
-          // console.log(colors.blue("getJSON( "+url+" ) error ->"));
-          // console.log(error);
-          resolve(error);
+        // console.log(colors.blue("getJSON( "+url+" ) error ->"));
+        // console.log(error);
+        resolve(error);
       });
     });
   }
 
-  async getRawContent(path){
-    return await new Promise((resolve)=>{
+  async getRawContent(path) {
+    return await new Promise((resolve) => {
       fileGetContents(path).then(content => {
-          // console.log(colors.blue("getJSON( "+url+" ) success ->"));
-          resolve(content);
+        // console.log(colors.blue("getJSON( "+url+" ) success ->"));
+        resolve(content);
       }).catch(error => {
-          // console.log(colors.blue("getJSON( "+url+" ) error ->"));
-          // console.log(error);
-          resolve(error);
+        // console.log(colors.blue("getJSON( "+url+" ) error ->"));
+        // console.log(error);
+        resolve(error);
       });
     });
   }
 
-  async postJSON(url, data){
-    return await new Promise((resolve)=>{
+  async postJSON(url, data) {
+    return await new Promise((resolve) => {
       fetch(url, {
-              method: 'post',
-              body:    JSON.stringify(data),
-              headers: { 'Content-Type': 'application/json' },
+        method: 'post',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' },
       }).then(res => {
-            res.json().then(json => {
-                  resolve(json);
-            }).catch(error => {
-                  resolve(error);
-            });
+        res.json().then(json => {
+          resolve(json);
+        }).catch(error => {
+          resolve(error);
+        });
       }).catch(error => {
-            resolve(error);
+        resolve(error);
       });
     });
   }
 
-  async getIP(indexIP){
+  async getIP(indexIP) {
 
     const sourceIP = [
       // ['http://ipecho.net/plain',false],
-      ['https://ident.me/',false],
-      ['https://v4.ident.me/',false],
-      ['https://api.ip.sb/ip',false],
-      ['https://api-ipv4.ip.sb/ip',false],
-      ['https://api.ipify.org/?format=json',true],
-      ['https://api64.ipify.org/?format=json',true]
+      ['https://ident.me/', false],
+      ['https://v4.ident.me/', false],
+      ['https://api.ip.sb/ip', false],
+      ['https://api-ipv4.ip.sb/ip', false],
+      ['https://api.ipify.org/?format=json', true],
+      ['https://api64.ipify.org/?format=json', true]
     ];
 
     const newAttempIP = async (resolve, error) => {
       indexIP++;
       console.log('timer 1.5s ...');
       await new Util().timer(1500);
-      if(indexIP<sourceIP.length){
-        console.log(colors.red('error ip url: '+sourceIP[indexIP-1][0]));
+      if (indexIP < sourceIP.length) {
+        console.log(colors.red('error ip url: ' + sourceIP[indexIP - 1][0]));
         console.log(error);
-        console.log(colors.yellow("getIP new attemp: "+sourceIP[indexIP][0]));
+        console.log(colors.yellow("getIP new attemp: " + sourceIP[indexIP][0]));
         resolve(await this.getIP(indexIP));
-      }else {
+      } else {
         resolve(await this.getIP(0));
       }
     };
 
-    return await new Promise((resolve)=>{
+    return await new Promise((resolve) => {
 
       indexIP == undefined ? indexIP = 0 : null;
 
       fileGetContents(sourceIP[indexIP][0]).then(async content => {
 
-          sourceIP[indexIP][1] == true ? content = JSON.parse(content)['ip'] : null;
+        sourceIP[indexIP][1] == true ? content = JSON.parse(content)['ip'] : null;
 
-          if(new Util().validateIP(content)){
-            console.log(colors.yellow('new ip:'+content+' date:'+new Date().toLocaleString()));
-            resolve(content);
-          }else{
-            await newAttempIP(resolve, 'not valid ip content');
-          }
+        if (new Util().validateIP(content)) {
+          console.log(colors.yellow('new ip:' + content + ' date:' + new Date().toLocaleString()));
+          resolve(content);
+        } else {
+          await newAttempIP(resolve, 'not valid ip content');
+        }
 
       }).catch(async error => {
 
@@ -103,7 +103,7 @@ export class RestService {
     });
   }
 
-  infoReq(req){
+  infoReq(req) {
     return {
       ip: (req.headers['x-forwarded-for'] || req.connection.remoteAddress),
       host: req.headers.host,
